e13e6db0eb4967cf72e43aa62332fa7abd846c25
Mathilde Grapin <mathilde.grapin@rte-france.com>
2021-07-30 17:02:30 +0200

    Add licence header
    Signed-off-by: Mathilde Grapin <mathilde.grapin@rte-france.com>

--- example/src/index.js
+++ example/src/index.js
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2021, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 import SldSvg from 'sld-svg/sldsvg.js'
 import pertapMetadata from './assets/diagrams/pertap3_france_metadata.json'
 import pertapSubstation from './assets/diagrams/pertap3_france.svg'
